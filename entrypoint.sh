2a79d37d4fa7863f294e073551b39d505f186a97
Heston Snodgrass <hsnodgrass3@gmail.com>
2022-03-15 17:39:50 -0700

    Add input for adding items to PDK validate ignorelist

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ cp /.gemfile "$HOME"/.gemfile
 
 cd "$GITHUB_WORKSPACE" || exit 1
 
+if [ -n "$2" ]; then
+    ignores=$(echo "$2" | tr "," "\n")
+    for ignore in $ignores; do
+        /opt/puppetlabs/pdk/bin/pdk set config project.validate.ignore "$ignore"
+    done
+fi
+
 /opt/puppetlabs/pdk/bin/pdk release "$1" --force
